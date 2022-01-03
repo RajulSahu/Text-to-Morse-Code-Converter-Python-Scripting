ae7f3cfd60fb0b3576e7c97c1424ee7c510bb2a5
Rajul Sahu <74370799+RajulSahu@users.noreply.github.com>
2022-01-03 22:39:14 +0530

    Developed Morse Code Generating code.

---
+++ Morse_Code_Generator.py
@@ -0,0 +1,53 @@
+MORSE_CODE_DICT = {
+    'A': '.-',
+    'B': '-...',
+    'C': '-.-.',
+    'D': '-..',
+    'E': '.',
+    'F': '..-.',
+    'G': '--.',
+    'H': '....',
+    'I': '..',
+    'J': '.---',
+    'K': '-.-',
+    'L': '.-..',
+    'M': '--',
+    'N': '-.',
+    'O': '---',
+    'P': '.--.',
+    'Q': '--.-',
+    'R': '.-.',
+    'S': '...',
+    'T': '-',
+    'U': '..-',
+    'V': '...-',
+    'W': '.--',
+    'X': '-..-',
+    'Y': '-.--',
+    'Z': '--..',
+    '1': '.----',
+    '2': '..---',
+    '3': '...--',
+    '4': '....-',
+    '5': '.....',
+    '6': '-....',
+    '7': '--...',
+    '8': '---..',
+    '9': '----.',
+    '0': '-----',
+}
+
+
+def encryptor(text):
+    encrypted = ""
+    for letter in text:
+        if letter != " ":
+            encrypted = encrypted + MORSE_CODE_DICT.get(letter) + " "
+        else:
+            encrypted = encrypted + " "
+    print(encrypted)
+
+
+print("\n\n\t\tThe Morse Code Generator!")
+text_to_encrypt = input("Enter the text to Encrypt : ").upper()
+encryptor(text_to_encrypt)
